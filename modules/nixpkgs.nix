3222980846d016829def761e0bde1f9c81f1c0bf
Ovyerus <ovy@ovyerus.com>
2024-06-30 16:27:47 +1000

    fix: use flake input for nixpkgs path

--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -1,14 +1,10 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: {
+{inputs, ...}: {
   # Make sure that the `nixpkgs` reference on the CLI refers to the system
   # nixpkgs so that it does not keep re-downloading the latest version every
   # time I want to run a single package
   nix.registry.nixpkgs.to = {
     type = "path";
-    path = pkgs.path;
+    path = inputs.nixpkgs;
     narHash = inputs.nixpkgs.narHash;
   };
 
