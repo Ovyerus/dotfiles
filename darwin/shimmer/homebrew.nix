9d8999b61d54c9c67838ce5101a91a11f49f06ce
Ovyerus <ovy@ovyerus.com>
2024-06-30 16:35:34 +1000

    shimmer: auto update & cleanup homebrew

--- darwin/shimmer/homebrew.nix
+++ darwin/shimmer/homebrew.nix
@@ -1,38 +1,45 @@
 {...}: {
-  homebrew.enable = true;
+  homebrew = {
+    enable = true;
+    # Update and cleanup homebrew packages when rebuilding.
+    global.autoUpdate = false;
+    onActivation.autoUpdate = true;
+    onActivation.upgrade = true;
+    onActivation.cleanup = "zap";
 
-  homebrew.taps = [
-    "homebrew/cask-versions"
-    "ovyerus/klog"
-    "ovyerus/tap"
-  ];
+    taps = [
+      "homebrew/cask-versions"
+      "ovyerus/klog"
+      "ovyerus/tap"
+    ];
 
-  homebrew.brews = [
-    "bitwarden-cli"
-    "klog"
-    "mas"
-  ];
+    brews = [
+      "bitwarden-cli"
+      "klog"
+      "mas"
+    ];
 
-  homebrew.casks = [
-    "discord"
-    "figma"
-    "httpie"
-    "insomnia"
-    "iterm2-beta"
-    "karabiner-elements"
-    "maccy"
-    "orbstack"
-    # Actually searches symlinks for apps, compared to Alfred.
-    "raycast"
-    "visual-studio-code"
-    # "vorta"
-  ];
+    casks = [
+      "discord"
+      "figma"
+      "httpie"
+      "insomnia"
+      "iterm2-beta"
+      "karabiner-elements"
+      "maccy"
+      "orbstack"
+      # Actually searches symlinks for apps, compared to Alfred.
+      "raycast"
+      "visual-studio-code"
+      # "vorta"
+    ];
 
-  homebrew.masApps = {
-    Bitwarden = 1352778147;
-    Keka = 470158793;
-    "Yubico Authenticator" = 1497506650;
-    "System Color Picker" = 1545870783;
-    Tailsacle = 1475387142;
+    masApps = {
+      Bitwarden = 1352778147;
+      Keka = 470158793;
+      "Yubico Authenticator" = 1497506650;
+      "System Color Picker" = 1545870783;
+      Tailsacle = 1475387142;
+    };
   };
 }
