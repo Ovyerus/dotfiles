b9795818082f6bc0bed8f9431e0103746b2ef01c
Ovyerus <ovy@ovyerus.com>
2024-07-08 13:03:07 +1000

    wallsocket: add xpadneo & adjust bluetooth latency

--- nixos/wallsocket/bluetooth.nix
+++ nixos/wallsocket/bluetooth.nix
@@ -1,4 +1,14 @@
 {pkgs, ...}: {
   hardware.bluetooth.enable = true;
+  hardware.bluetooth.settings = {
+    # Try and reduce latency from Xbox Series controllers
+    # https://atar-axis.github.io/xpadneo#high-latency-or-lost-button-events-with-bluetooth-le
+    LE = {
+      MinConnectionInterval = 7;
+      MaxConnectionInterval = 9;
+      ConnectionLatency = 0;
+    };
+  };
+  hardware.xpadneo.enable = true;
   environment.systemPackages = [pkgs.kdePackages.bluedevil];
 }
