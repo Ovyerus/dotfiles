6a8e22433483549e83777316a0ac313341b90ef5
Ovyerus <ovy@ovyerus.com>
2024-08-09 21:20:49 +1000

    wallsocket: add lact

--- nixos/wallsocket/graphics.nix
+++ nixos/wallsocket/graphics.nix
@@ -1,6 +1,16 @@
-{...}: {
+{pkgs, ...}: {
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
+    extraPackages = [pkgs.amdvlk];
+  };
+
+  environment.systemPackages = [pkgs.lact];
+
+  systemd.services.lactd = {
+    description = "AMDGPU Control Daemon";
+    enable = true;
+    serviceConfig.ExecStart = "${pkgs.lact}/bin/lact daemon";
+    wantedBy = ["multi-user.target"];
   };
 }
