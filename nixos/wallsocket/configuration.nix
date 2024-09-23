bf425ae653eca30b0feba8ed7d056cc888a92122
Ovyerus <ovy@ovyerus.com>
2024-09-23 22:53:50 +1000

    wallsocket: enable resolved and mullvad

--- nixos/wallsocket/configuration.nix
+++ nixos/wallsocket/configuration.nix
@@ -155,7 +155,17 @@
   '';
 
   programs.fish.enable = true;
-  # services.mullvad-vpn.enable = true;
+  services.mullvad-vpn.enable = true;
+  services.mullvad-vpn.package = pkgs.mullvad-vpn;
+
+  networking.nameservers = ["1.1.1.1" "1.0.0.1"];
+  services.resolved = {
+    enable = true;
+    # dnssec = "true";
+    domains = ["~."];
+    fallbackDns = ["1.1.1.1" "1.0.0.1"];
+    # dnsovertls = "trues";
+  };
 
   fonts = {
     packages = [
