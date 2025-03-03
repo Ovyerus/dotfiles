3cd3fa8352ffd4f793b6d97a471d71a251aa637e
Ovyerus <ovy@ovyerus.com>
2025-03-03 22:05:39 +1100

    desktop: add 1password ssh agent

--- home/modules/ssh.nix
+++ home/modules/ssh.nix
@@ -1,10 +1,18 @@
-{
+{lib, ...}: {
   programs.ssh = {
     enable = true;
-    matchBlocks."*" = {
-      identitiesOnly = true;
-      identityFile = "~/.ssh/id_ed25519_sk_rk";
-      user = "ovy";
+    matchBlocks = {
+      "*" = {
+        identitiesOnly = true;
+        identityFile = "~/.ssh/id_ed25519_sk_rk";
+        user = "ovy";
+      };
+      # OpenWrt
+      "192.168.1.1" = lib.hm.dag.entryBefore ["*"] {
+        user = "root";
+        extraOptions.IdentityAgent = "~/.1password/agent.sock";
+        extraOptions.IdentitiesOnly = "no";
+      };
     };
   };
 }
