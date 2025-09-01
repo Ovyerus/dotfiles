0ddd9e5e5687dfdc03f8e3e442c90b4fb1fe4d12
Ashlynne Mitchell <ovy@ovyerus.com>
2025-09-01 12:39:47 +1000

    chore: disable xpadneo

--- modules/bluetooth.nix
+++ modules/bluetooth.nix
@@ -11,16 +11,16 @@ delib.module {
   nixos.ifEnabled = {
     hardware.bluetooth.enable = true;
     # TODO: determine if this actually affected me. Maybe should yeet.
-    hardware.bluetooth.settings = {
-      # Try and reduce latency from Xbox Series controllers
-      # https://atar-axis.github.io/xpadneo#high-latency-or-lost-button-events-with-bluetooth-le
-      LE = {
-        MinConnectionInterval = 7;
-        MaxConnectionInterval = 9;
-        ConnectionLatency = 0;
-      };
-    };
-    hardware.xpadneo.enable = true;
+    # hardware.bluetooth.settings = {
+    #   # Try and reduce latency from Xbox Series controllers
+    #   # https://atar-axis.github.io/xpadneo#high-latency-or-lost-button-events-with-bluetooth-le
+    #   LE = {
+    #     MinConnectionInterval = 7;
+    #     MaxConnectionInterval = 9;
+    #     ConnectionLatency = 0;
+    #   };
+    # };
+    # hardware.xpadneo.enable = true;
     environment.systemPackages = [pkgs.kdePackages.bluedevil];
   };
 }
--- modules/graphics.nix
+++ modules/graphics.nix
@@ -17,6 +17,7 @@ delib.module {
     };
 
     environment.systemPackages = with pkgs; [amdgpu_top lact nvtopPackages.full];
+    environment.sessionVariables.KWIN_DRM_NO_AMS = "1";
 
     systemd.services.lactd = {
       description = "AMDGPU Control Daemon";
